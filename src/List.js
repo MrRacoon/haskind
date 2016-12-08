f771fb601ae6c3b5c01a0c449f43190586d813e0
Erik Sutherland <erik.misu@gmail.com>
2016-12-07 19:51:11 -0800

    Troved through Data.List
    Added a bunch of tests, and left more test skeleton

--- src/List.js
+++ src/List.js
@@ -7,6 +7,8 @@ const notUndefined = (x, fn) => {
   return typeof x === 'undefined' ? fn() : x
 }
 
+// Basic functions
+
 export const head: Function =
   ls => notUndefined(ls[0], error('List.head: empty list'))
 
@@ -25,7 +27,7 @@ const _lengthArray : Function =
 const _lengthObject: Function =
   (ls: {}) => Object.keys(ls).length
 
-export const length: Function = (ls: any) => {
+export const length: Function = function _length(ls: any) {
 
   if (Array.isArray(ls))
     return _lengthArray(ls)
@@ -34,11 +36,47 @@ export const length: Function = (ls: any) => {
     return _lengthObject(ls)
 }
 
+// List transformations
+
 export const map: any[] = (fn: Function, ls: any[]) =>
   ls.map(fn)
+
+export const reverse: any[] = (ls: any[]) =>
+  ls.reduce((acc, x) => x + acc, [])
+
+export const intersperse: any[] = (ch: String, ls: any[]) =>
+  ls.reduce((acc, x) => acc.list ? acc + x : acc + ch + x, '')
+
+// Reducing lists (folds)
+
+export const foldl: any[] = (fn: Function, init: any, ls: any[]) =>
+  ls.reduce(fn, init)
+
+// Special folds
+
+// Building lists
+
+// Accumulating maps
+
+// Unfolding
+
+// Extracting sublists
+
+// Predicates
+
+// Searching by equality
+
+// Searching with a predicate
 
 export const filter: any[] = (fn: Function, ls: any[]) =>
   ls.filter(fn)
 
-export const foldl: any[] = (fn: Function, init: any, ls: any[]) =>
-  ls.reduce(fn, init)
+// Indexing lists
+
+// Zipping and unzipping lists
+
+// Functions on strings
+
+// "Set" operations
+
+// Ordered lists
--- test/List.js
+++ test/List.js
@@ -4,98 +4,346 @@ import should from 'should'
 const id = a => a
 
 describe('List', () => {
-  describe('head', () => {
-    it('head([1,2,3])', () => {
-      L.head([1, 2, 3]).should.be.eql(1)
+  describe('Basic Functions', () => {
+    describe('head', () => {
+      it('([])', () => {
+        (() => { L.head([]) }).should.throw('haskind.List.head: empty list')
+      })
+      it('([1])', () => {
+        L.head([1]).should.be.eql(1)
+      })
+      it('([1,2])', () => {
+        L.head([1, 2]).should.be.eql(1)
+      })
+      it('([1,2,3])', () => {
+        L.head([1, 2, 3]).should.be.eql(1)
+      })
     })
-    it('head([1,2])', () => {
-      L.head([1, 2]).should.be.eql(1)
+    describe('last', () => {
+      it('([])', () => {
+        (() => { L.last([]) }).should.throw('haskind.List.last: empty list')
+      })
+      it('([1])', () => {
+        L.last([1]).should.be.eql(1)
+      })
+      it('([1,2])', () => {
+        L.last([1, 2]).should.be.eql(2)
+      })
+      it('([1,2,3])', () => {
+        L.last([1, 2, 3]).should.be.eql(3)
+      })
     })
-    it('head([1])', () => {
-      L.head([1]).should.be.eql(1)
+    describe('tail', () => {
+      it('([])', () => {
+        L.tail([]).should.be.eql([])
+      })
+      it('([1])', () => {
+        L.tail([1]).should.be.eql([])
+      })
+      it('([1,2])', () => {
+        L.tail([1,2]).should.be.eql([2])
+      })
+      it('([1,2,3])', () => {
+        L.tail([1,2,3]).should.be.eql([2,3])
+      })
     })
-    it('head([])', () => {
-      (() => { L.head([]) }).should.throw('haskind.List.head: empty list')
+    describe('init', () => {
+      it('([])', () => {
+        (() => { L.init([]) }).should.throw('haskind.List.init: empty list')
+      })
+      it('([1])', () => {
+        L.init([1]).should.be.eql([])
+      })
+      it('([1,2])', () => {
+        L.init([1, 2]).should.be.eql([1])
+      })
+      it('([1,2,3])', () => {
+        L.init([1, 2, 3]).should.be.eql([1,2])
+      })
+    })
+    describe('length', () => {
+      it('([])', () => {
+        should(L.length([])).be.eql(0)
+      })
+      it('({})', () => {
+        should(L.length({})).be.eql(0)
+      })
+      it('([1])', () => {
+        should(L.length([1])).be.eql(1)
+      })
+      it('([1,2])', () => {
+        should(L.length([1,2])).be.eql(2)
+      })
+      it('([1,2,3])', () => {
+        should(L.length([1,2,3])).be.eql(3)
+      })
+      it('({ "1" : 1 })', () => {
+        should(L.length([1])).be.eql(1)
+      })
+      it('({ "1" : 1, "2": 3 })', () => {
+        should(L.length({ '1': 1, '2': 3 })).be.eql(2)
+      })
     })
   })
-  describe('last', () => {
-    it('([1,2,3])', () => {
-      L.last([1, 2, 3]).should.be.eql(3)
+  describe('List transformations', () => {
+    describe('map', () => {
+      it('(id, [1,2,3])', () => {
+        L.map(id, [1,2,3]).should.eql([1,2,3])
+      })
+      it('(succ, [1,2,3])', () => {
+        const fn = x => x + 1
+        L.map(fn, [1,2,3]).should.eql([2,3,4])
+      })
     })
-    it('([1,2])', () => {
-      L.last([1, 2]).should.be.eql(2)
+    describe('reverse', () => {
+      it('([])', () => {
+        L.reverse([]).should.be.eql([])
+      })
+      it('([1])', () => {
+        L.reverse([1]).should.be.eql([1])
+      })
+      it('([1,2])', () => {
+        L.reverse([1,2]).should.be.eql([2,1])
+      })
+      it('([1,2,3])', () => {
+        L.reverse([1,2,3]).should.be.eql([3,2,1])
+      })
     })
-    it('([1])', () => {
-      L.last([1]).should.be.eql(1)
-    })
-    it('([])', () => {
-      (() => { L.last([]) }).should.throw('haskind.List.last: empty list')
-    })
-  })
-  describe('tail', () => {
-    it('([1,2,3])', () => {
-      L.tail([1,2,3]).should.be.eql([2,3])
-    })
-    it('([1,2])', () => {
-      L.tail([1,2]).should.be.eql([2])
-    })
-    it('([1])', () => {
-      L.tail([1]).should.be.eql([])
-    })
-    it('([])', () => {
-      L.tail([]).should.be.eql([])
-    })
-  })
-  describe('init', () => {
-    it('([1,2,3])', () => {
-      L.init([1, 2, 3]).should.be.eql([1,2])
-    })
-    it('([1,2])', () => {
-      L.init([1, 2]).should.be.eql([1])
-    })
-    it('([1])', () => {
-      L.init([1]).should.be.eql([])
-    })
-    it('([])', () => {
-      (() => { L.init([]) }).should.throw('haskind.List.init: empty list')
+    describe('intersperse', () => {
+      it('(",", "000")', () => {
+        L.intersperse(',', '000').should.be.eql('01010')
+      })
+      it('("1", ["0", "0", "0"])', () => {
+        L.intersperse('1', ['0', '0', '0']).should.be.eql(['0', '1', '0', '1', '0'])
+      })
+      it('(",", ["0", "0", "0"])', () => {
+        L.intersperse(',', ['0', '0', '0']).should.be.eql(['0', ',', '0', ',', '0'])
+      })
     })
   })
+  describe('Reducing lists (folds)', () => {
+    describe('foldl', () => {
+      it('(id, 0, [1,2,3])', () => {
+        L.foldl(id, 0, [1,2,3]).should.eql(0)
+      })
+      it('(add, 0, [1,2,3])', () => {
+        const fn = (acc, x) => acc + x
+        L.foldl(fn, 0, [1,2,3]).should.eql(6)
+      })
+    })
+  })
+  describe('Special folds', () => {
+    describe('concat', () => {
+      it('([[1], [2,3], [], [[1], []]])', () => {
+        L.concat([[1], [2,3], [], [[1], []]]).should.be.eql([1,2,3,[1],[]])
+      })
+    })
+    describe('and', () => {
+      it('([])', () => {
+        L.and([]).should.be.eql(true)
+      })
+      it('([true])', () => {
+        L.and([true]).should.be.eql(true)
+      })
+      it('([true, true])', () => {
+        L.and([true, true]).should.be.eql(true)
+      })
+      it('([false, true])', () => {
+        L.and([false, true]).should.be.eql(false)
+      })
+      it('([true, false])', () => {
+        L.and([true, false]).should.be.eql(false)
+      })
+      it('([false, false])', () => {
+        L.and([false, false]).should.be.eql(false)
+      })
+    })
+    describe('or', () => {
+      it('([])', () => {
+        L.or([]).should.be.eql(false)
+      })
+      it('([true])', () => {
+        L.or([true]).should.be.eql(true)
+      })
+      it('([true, true])', () => {
+        L.or([true, true]).should.be.eql(true)
+      })
+      it('([false, true])', () => {
+        L.or([false, true]).should.be.eql(true)
+      })
+      it('([true, false])', () => {
+        L.or([true, false]).should.be.eql(true)
+      })
+      it('([false, false])', () => {
+        L.or([false, false]).should.be.eql(false)
+      })
+    })
+    describe('any', () => {
+      it('(id, [])', () => {
+        L.any(id, []).should.be.eql(false)
+      })
+      it('(id, [true])', () => {
+        L.any(id, [true]).should.be.eql(true)
+      })
+      it('(id, [true, true])', () => {
+        L.any(id, [true, true]).should.be.eql(true)
+      })
+      it('(id, [false, true])', () => {
+        L.any(id, [false, true]).should.be.eql(true)
+      })
+      it('(id, [true, false])', () => {
+        L.any(id, [true, false]).should.be.eql(true)
+      })
+      it('(id, [false, false])', () => {
+        L.any(id, [false, false]).should.be.eql(false)
+      })
+    })
+    describe('all', () => {
+      it('(id, [])', () => {
+        L.all([]).should.be.eql(false)
+      })
+      it('(id, [true])', () => {
+        L.all(id, [true]).should.be.eql(true)
+      })
+      it('(id, [true, true])', () => {
+        L.all(id, [true, true]).should.be.eql(true)
+      })
+      it('(id, [false, true])', () => {
+        L.all(id, [false, true]).should.be.eql(true)
+      })
+      it('(id, [true, false])', () => {
+        L.all(id, [true, false]).should.be.eql(true)
+      })
+      it('(id, [false, false])', () => {
+        L.all(id, [false, false]).should.be.eql(false)
+      })
+    })
+    describe('sum', () => {
+      it('([1,1,1])', () => {
+        L.sum([1,1,1]).should.be.eql(3)
+      })
+      it('([1,2,3])', () => {
+        L.sum([1,2,3]).should.be.eql(6)
+      })
+      it('([1,2,3,4])', () => {
+        L.sum([1,2,3,4]).should.be.eql(10)
+      })
+    })
+    describe('product', () => {
+      it('([1,1,1])', () => {
+        L.product([1,1,1]).should.be.eql(1)
+      })
+      it('([1,2,3])', () => {
+        L.product([1,2,3]).should.be.eql(6)
+      })
+      it('([1,2,3,4])', () => {
+        L.product([1,2,3,4]).should.be.eql(24)
+      })
+    })
+    describe('maximum', () => {
+      it('([1,1,1])', () => {
+        L.maximum([1,1,1]).should.be.eql(1)
+      })
+      it('([1,2,3])', () => {
+        L.maximum([1,2,3]).should.be.eql(3)
+      })
+      it('([1,2,3,4])', () => {
+        L.maximum([1,2,3,4]).should.be.eql(4)
+      })
+      it('([1,8,3,4])', () => {
+        L.maximum([1,8,3,4]).should.be.eql(8)
+      })
+    })
+    describe('minimum', () => {
+      it('([1,1,1])', () => {
+        L.minimum([1,1,1]).should.be.eql(1)
+      })
+      it('([1,2,3])', () => {
+        L.minimum([1,2,3]).should.be.eql(1)
+      })
+      it('([1,2,3,4])', () => {
+        L.minimum([1,2,3,4]).should.be.eql(1)
+      })
+      it('([1,8,3,4])', () => {
+        L.minimum([1,8,3,4]).should.be.eql(1)
+      })
+    })
+  })
+  describe('Building lists', () => {
+  })
+  describe('Accumulating Maps', () => {
+  })
+  describe('Unfolding', () => {
+  })
+  describe('Extracting sublists', () => {
+    describe('take', () => {
 
-  describe('length', () => {
-    it('works', () => {
-      should(L.length([])).be.eql(0)
-      should(L.length({})).be.eql(0)
+    })
+    describe('drop', () => {
 
-      L.length([1]).should.be.eql(1)
-      L.length([1, 2, 3]).should.be.eql(3)
-      L.length({ '1': 1 }).should.be.eql(1)
     })
   })
-  describe('map', () => {
-    it('(id, [1,2,3])', () => {
-      L.map(id, [1,2,3]).should.eql([1,2,3])
+  describe('Predicates', () => {
+  })
+  describe('Searching by equality', () => {
+    describe('elem', () => {
+
     })
-    it('(succ, [1,2,3])', () => {
-      const fn = x => x + 1
-      L.map(fn, [1,2,3]).should.eql([2,3,4])
+    describe('notElem', () => {
+
+    })
+    describe('lookup', () => {
+
     })
   })
-  describe('filter', () => {
-    it('(id, [1,2,3])', () => {
-      L.filter(id, [1,2,3]).should.eql([1,2,3])
+  describe('Searching with a predicate ', () => {
+    describe('find', () => {
+
     })
-    it('(isEven, [1,2,3])', () => {
-      const fn = x => x % 2 === 0
-      L.filter(fn, [1,2,3]).should.eql([2])
+    describe('filter', () => {
+      it('(id, [1,2,3])', () => {
+        L.filter(id, [1,2,3]).should.eql([1,2,3])
+      })
+      it('(isEven, [1,2,3])', () => {
+        const fn = x => x % 2 === 0
+        L.filter(fn, [1,2,3]).should.eql([2])
+      })
+    })
+    describe('partition', () => {
+
     })
   })
-  describe('foldl', () => {
-    it('(id, 0, [1,2,3])', () => {
-      L.foldl(id, 0, [1,2,3]).should.eql(0)
+  describe('Zipping and unzipping lists', () => {
+    describe('zip', () => {
+
     })
-    it('(add, 0, [1,2,3])', () => {
-      const fn = (acc, x) => acc + x
-      L.foldl(fn, 0, [1,2,3]).should.eql(6)
+  })
+  describe('Functions on strings', () => {
+    describe('lines', () => {
+
+    })
+    describe('words', () => {
+
+    })
+    describe('unlines', () => {
+
+    })
+    describe('unwords', () => {
+
+    })
+  })
+  describe('"Set" operations', () => {
+    describe('nub', () => {
+
+    })
+    describe('delete\'', () => {
+
+    })
+    describe('union', () => {
+
+    })
+    describe('intersect', () => {
+
     })
   })
 })
