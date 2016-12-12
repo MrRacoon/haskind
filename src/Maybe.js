ed19e7641fbd665466d8281ca9fbea78f9e8c67e
Erik Sutherland <erik.misu@gmail.com>
2016-12-11 21:44:31 -0800

    sharpen the Maybe type decl

--- src/Maybe.js
+++ src/Maybe.js
@@ -1,21 +1,22 @@
 import type from 'type-of'
 import { error } from './util'
 
-export type Maybe = { just?: any, nothing?: void }
+export type A = any;
+export type Maybe<A> = { just?: A, nothing?: void }
 
 export const Just: Maybe = (x) => ({ just: x })
 export const Nothing: Maybe = () => ({ nothing: null })
 
-export const maybe = (def: any, fn: Function, m: Maybe) => {
+export const maybe = (def: any, fn: Function, m: Maybe<any>) => {
   if (isNothing(m)) return def
   if (isJust(m)) return fn(m.just)
   return error('Maybe.maybe: third argument should have type maybe')
 }
 
-export const isJust: Function = (m: Maybe): boolean =>
+export const isJust: Function = (m: Maybe<any>): boolean =>
   type(m) === 'object' && type(m.just) !== 'undefined' && type(m.nothing) === 'undefined'
 
-export const isNothing: Function = (m: Maybe): boolean =>
+export const isNothing: Function = (m: Maybe<any>): boolean =>
   type(m) === 'object' && type(m.nothing) !== 'undefined' && type(m.just) === 'undefined'
 
 
