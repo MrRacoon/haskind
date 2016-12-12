0e6b11bd4b14b016b89b92b468695fc848bbb132
Erik Sutherland <erik.misu@gmail.com>
2016-12-11 21:37:41 -0800

    made my own Maybe type, stripped out ramda-fantasy

--- src/Maybe.js
+++ src/Maybe.js
@@ -1,4 +1,27 @@
-import { Maybe } from 'ramda-fantasy'
+import type from 'type-of'
+import { error } from './util'
 
-export const maybe = (def, fn, m) =>
-  Maybe.maybe(def, fn, m)
+export type Maybe = { just?: any, nothing?: void }
+
+export const Just: Maybe = (x) => ({ just: x })
+export const Nothing: Maybe = () => ({ nothing: null })
+
+export const maybe = (def: any, fn: Function, m: Maybe) => {
+  if (isNothing(m)) return def
+  if (isJust(m)) return fn(m.just)
+  return error('Maybe.maybe: third argument should have type maybe')
+}
+
+export const isJust: Function = (m: Maybe): boolean =>
+  type(m) === 'object' && type(m.just) !== 'undefined' && type(m.nothing) === 'undefined'
+
+export const isNothing: Function = (m: Maybe): boolean =>
+  type(m) === 'object' && type(m.nothing) !== 'undefined' && type(m.just) === 'undefined'
+
+
+// fromJust :: Maybe a -> a
+// fromMaybe :: a -> Maybe a -> a
+// listToMaybe :: [a] -> Maybe a
+// maybeToList :: Maybe a -> [a]
+// catMaybes :: [Maybe a] -> [a]
+// mapMaybe :: (a -> Maybe b) -> [a] -> [b]
--- test/Maybe.js
+++ test/Maybe.js
@@ -1,22 +1,22 @@
 // import should from 'should'
 import * as M from '../src/Maybe'
 import { id } from '../src/util'
-import { Maybe } from 'ramda-fantasy'
 
+const { Just, Nothing } = M
 
 describe('Maybe', () => {
   describe('maybe', () => {
-    it('(1, id, Maybe.Just(4))', () => {
-      M.maybe(1, id, Maybe.Just(4)).should.eql(4)
+    it('(1, id, Just(4))', () => {
+      M.maybe(1, id, Just(4)).should.eql(4)
     })
-    it('(1, id, Maybe.Nothing())', () => {
-      M.maybe(1, id, Maybe.Nothing()).should.eql(1)
+    it('(1, id, Nothing())', () => {
+      M.maybe(1, id, Nothing()).should.eql(1)
     })
-    it('(1, inc, Maybe.Just(4))', () => {
-      M.maybe(1, x => x+1, Maybe.Just(4)).should.eql(5)
+    it('(1, inc, Just(4))', () => {
+      M.maybe(1, x => x+1, Just(4)).should.eql(5)
     })
-    it('(1, inc, Maybe.Nothing())', () => {
-      M.maybe(1, x => x+1, Maybe.Nothing()).should.eql(1)
+    it('(1, inc, Nothing())', () => {
+      M.maybe(1, x => x+1, Nothing()).should.eql(1)
     })
   })
 })
