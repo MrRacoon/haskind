771d9489636d7d58ff03147ccc28573f9fee0222
Erik Sutherland <erik.misu@gmail.com>
2016-12-12 21:52:20 -0800

    fill out more Maybe tests, tdd

--- src/Maybe.js
+++ src/Maybe.js
@@ -1,29 +1,34 @@
 import type from 'type-of'
 import { error } from './util'
 
-export type A = any
-export type Maybe<A> = _Just<A> | Nothing
-export type _Just<A> = { just: A }
+export type Maybe = _Just<any> | Nothing
+export type _Just = { just: any }
 export type _Nothing = { nothing: void }
 
 export const Just: Maybe = (x) => ({ just: x })
 export const Nothing: Maybe = () => ({ nothing: null })
 
-export const maybe = (def: any, fn: Function, m: Maybe<any>) => {
+export const maybe = (def: any, fn: Function, m: Maybe) => {
   if (isNothing(m)) return def
   if (isJust(m)) return fn(m.just)
   return error('Maybe.maybe: third argument should have type maybe')
 }
 
-export const isJust: Function = (m: Maybe<any>): boolean =>
+export const isJust: Function = (m: Maybe): boolean =>
   type(m) === 'object' && type(m.just) !== 'undefined' && type(m.nothing) === 'undefined'
 
-export const isNothing: Function = (m: Maybe<any>): boolean =>
+export const isNothing: Function = (m: Maybe): boolean =>
   type(m) === 'object' && type(m.nothing) !== 'undefined' && type(m.just) === 'undefined'
 
 
 // fromJust :: Maybe a -> a
+export const fromJust = (m: Maybe): any =>
+  isJust(m) ? m.just : error('Maybe.fromJust: Nothing')
+
 // fromMaybe :: a -> Maybe a -> a
+export const fromMaybe = (def: any, m: Maybe): any =>
+  isJust(m) ? m.just : def
+
 // listToMaybe :: [a] -> Maybe a
 // maybeToList :: Maybe a -> [a]
 // catMaybes :: [Maybe a] -> [a]
--- test/Maybe.js
+++ test/Maybe.js
@@ -1,4 +1,4 @@
-// import should from 'should'
+import should from 'should'
 import * as M from '../src/Maybe'
 import { id } from '../src/util'
 
@@ -20,27 +20,62 @@ describe('Maybe', () => {
     })
   })
   describe('isJust', () => {
-
+    it('(Just(42))', () => {
+      M.isJust(Just(42)).should.be.eql(true)
+    })
+    it('(Nothing())', () => {
+      M.isJust(Nothing()).should.be.eql(false)
+    })
   })
   describe('isNothing', () => {
-
+    it('(Just(42))', () => {
+      M.isNothing(Just(42)).should.be.eql(false)
+    })
+    it('(Nothing())', () => {
+      M.isNothing(Nothing()).should.be.eql(true)
+    })
   })
   describe('fromJust', () => {
-
+    it('(Just(42))', () => {
+      M.fromJust(Just(42)).should.be.eql(42)
+    })
+    it('(Nothing())', () => {
+      (() => M.fromJust(Nothing())).should.throw('asdf')
+    })
   })
   describe('fromMaybe', () => {
-
+    it('(12, Just(42))', () => {
+      M.fromMaybe(12, Just(42)).should.be.eql(42)
+    })
+    it('(12, Nothing())', () => {
+      M.fromMaybe(12, Nothing()).should.be.eql(12)
+    })
   })
   describe('listToMaybe', () => {
-
+    it('([42])', () => {
+      M.listToMaybe([42]).should.be.eql(Just(42))
+    })
+    it('([])', () => {
+      M.listToMaybe([]).should.be.eql(Nothing())
+    })
   })
   describe('maybeToList', () => {
-
+    it('(Just(42))', () => {
+      M.maybeToList(Just(42)).should.be.eql([42])
+    })
+    it('(Nothing())', () => {
+      M.maybeToList(Nothing()).should.be.eql([])
+    })
   })
   describe('catMaybes', () => {
-
+    it('([Just(42), Nothing])', () => {
+      M.catMaybes([Just(42), Nothing()]).should.be.eql([42])
+    })
   })
   describe('mapMaybe', () => {
-
+    it('(maybeEven, [1,2,3])', () => {
+      const maybeEven = (x) => x % 2 === 0 ? Just(x) : Nothing()
+      M.mapMaybe(maybeEven, [1,2,3]).should.be.eql([2])
+    })
   })
 })
