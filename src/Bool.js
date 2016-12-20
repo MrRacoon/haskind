6829969dbbd770d12efde35baa1a047b0ec63da1
Erik Sutherland <erik.misu@gmail.com>
2016-12-19 21:55:01 -0800

    add basic Bool functions

--- src/Bool.js
+++ src/Bool.js
@@ -1,7 +1,9 @@
 
 // and :: Bool -> Bool -> Bool
+export const and: Function = (x: any, y: any): boolean => x && y
 
 // or :: Bool -> Bool -> Bool
+export const or: Function = (x: any, y: any): boolean => x || y
 
 // not :: Bool -> Bool
 export const not: Function = (x: any): boolean => !x
@@ -10,3 +12,4 @@ export const not: Function = (x: any): boolean => !x
 export const otherwise: boolean = true
 
 // bool :: a -> a -> Bool -> a
+export const bool: Function = (x: any, y: any, p: any): any => p ? y : x
