5174758f340418eb971bf2441e43ebb7dd20500e
Erik Sutherland <erik.misu@gmail.com>
2017-01-03 23:02:22 -0800

    add flow types to Ix

--- src/Ix.js
+++ src/Ix.js
@@ -1,18 +1,18 @@
 import { _curry, error } from './util';
 
 // range :: (a, a) -> [a]
-export const range = ([a, b]) =>
+export const range = ([a: number, b: number]): number[] =>
   [...Array(b-a).keys()].map(x => x + a);
 
 // inRange :: (a, a) -> a -> Bool
 export const inRange = _curry(
-  ([a, b], i) =>
+  ([a: number, b: number], i: number): boolean =>
     a <= i && i <= b
 );
 
 // index :: (a, a) -> a -> Int
 export const index = _curry(
-  ([a, b], i) =>
+  ([a: number, b: number], i: number): number =>
     inRange([a,b], i)
       ? i - a
       : error('Ix.index: index out of range')
@@ -20,5 +20,5 @@ export const index = _curry(
 
 // rangeSize :: (a, a) -> Int
 export const rangeSize =
-  ([a, b]) =>
+  ([a: number, b: number]): number =>
     b - a;
