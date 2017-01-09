0d52d8456f76bdfa8e90633495fa74fabbf4028a
Erik Sutherland <erik.misu@gmail.com>
2017-01-08 20:52:32 -0800

    partially fill out Data.Function

--- test/Function.js
+++ test/Function.js
@@ -2,6 +2,10 @@ import should from 'should';
 import { Data } from '.';
 const { Function } = Data;
 
+const inc = x => x + 1;
+const add = (a,b) => a + b;
+const mult = (a,b) => a * b;
+
 describe('Function .', () => {
   describe('id', () => {
     describe('(1)', () => {
@@ -62,9 +66,7 @@ describe('Function .', () => {
       });
     });
   });
-  xdescribe('compose', function () {
-    const inc = x => x + 1;
-    const add = (a,b) => a + b;
+  describe('compose', function () {
     describe('(inc, add)(1,1)', function () {
       it('== 3', function () {
         Function.compose(inc, add)(1,1).should.be.eql(3);
@@ -91,38 +93,66 @@ describe('Function .', () => {
       });
     });
   });
-  xdescribe('pipe', function () {
-    const inc = x => x + 1;
-    const add = (a,b) => a + b;
-    describe('(add, inc)(1,1)', function () {
+  describe('pipe', function () {
+    describe('([add, inc])([1,1])', function () {
       it('== 3', function () {
-        Function.pipe(add, inc)(1,1).should.be.eql(3);
+        Function.pipe([add, inc])([1,1]).should.be.eql(3);
       });
     });
-    describe('(add, inc, inc)(1,1)', function () {
+    describe('([add, inc, inc])([1,1])', function () {
       it('== 4', function () {
-        Function.pipe(add, inc, inc)(1,1).should.be.eql(4);
+        Function.pipe([add, inc, inc])([1,1]).should.be.eql(4);
       });
     });
-    describe('(inc)(0)', function () {
+    describe('([inc])([0])', function () {
       it('== 1', function () {
-        Function.pipe(inc)(0).should.be.eql(1);
+        Function.pipe([inc])([0]).should.be.eql(1);
       });
     });
-    describe('(inc, inc)(0)', function () {
+    describe('([inc, inc])([0])', function () {
       it('== 2', function () {
-        Function.pipe(inc, inc)(0).should.be.eql(2);
+        Function.pipe([inc, inc])([0]).should.be.eql(2);
       });
     });
-    describe('(inc, inc, inc)(0)', function () {
+    describe('([inc, inc, inc])([0])', function () {
       it('== 3', function () {
-        Function.pipe(inc, inc, inc)(0).should.be.eql(3);
+        Function.pipe([inc, inc, inc])([0]).should.be.eql(3);
       });
     });
   });
-  xdescribe('comp', () => {
+  describe('comp', () => {
+    describe('([inc, add])([1,1])', function () {
+      it('== 3', function () {
+        Function.comp([inc, add])([1,1]).should.be.eql(3);
+      });
+    });
+    describe('([inc, inc, add])([1,1])', function () {
+      it('== 4', function () {
+        Function.comp([inc, inc, add])([1,1]).should.be.eql(4);
+      });
+    });
+    describe('([inc], [0])', function () {
+      it('== 1', function () {
+        Function.comp([inc], [0]).should.be.eql(1);
+      });
+    });
+    describe('([inc])([0])', function () {
+      it('== 1', function () {
+        Function.comp([inc])([0]).should.be.eql(1);
+      });
+    });
+    describe('([inc, inc])([0])', function () {
+      it('== 2', function () {
+        Function.comp([inc, inc])([0]).should.be.eql(2);
+      });
+    });
+    describe('([inc, inc, inc])([0])', function () {
+      it('== 3', function () {
+        Function.comp([inc, inc, inc])([0]).should.be.eql(3);
+      });
+    });
   });
-  xdescribe('flip', () => {
+  describe('flip', () => {
     describe('(const_, 1, 2)', () => {
       it('== 2', () => {
         Function.flip(Function.const_, 1, 2).should.be.eql(2);
@@ -149,6 +179,23 @@ describe('Function .', () => {
       });
     });
   });
+  describe('apply', () => {
+    describe('(0, inc)', () => {
+      it('== 1', () => {
+        Function.apply(0, inc).should.be.eql(1);
+      });
+    });
+    describe('(1, inc)', () => {
+      it('== 2', () => {
+        Function.apply(1, inc).should.be.eql(2);
+      });
+    });
+    describe('(1)(inc)', () => {
+      it('== 2', () => {
+        Function.apply(1)(inc).should.be.eql(2);
+      });
+    });
+  });
   xdescribe('fix', () => {
     describe('(const_(1))', () => {
       it('== 1', () => {
@@ -156,10 +203,60 @@ describe('Function .', () => {
       });
     });
   });
-  xdescribe('on', () => {
-    describe('(mult, inc, 1, 1)', () => {
-      it('== 4', () => {
-        Function.on((a,b) => a*b, x => x+1, 1, 1).should.be.eql(4);
+  describe('on', () => {
+    describe('(add, inc, 1, 2)', () => {
+      it('== 5', () => {
+        Function.on(add, inc, 1, 2).should.be.eql(5);
+      });
+    });
+    describe('(mult, inc, 1, 2)', () => {
+      it('== 6', () => {
+        Function.on(mult, inc, 1, 2).should.be.eql(6);
+      });
+    });
+    describe('(add, inc, 2, 4)', () => {
+      it('== 8', () => {
+        Function.on(add, inc, 2, 4).should.be.eql(8);
+      });
+    });
+    describe('(mult, inc, 2, 4)', () => {
+      it('== 6', () => {
+        Function.on(mult, inc, 2, 4).should.be.eql(15);
+      });
+    });
+    describe('(mult)(inc, 2, 4)', () => {
+      it('== 6', () => {
+        Function.on(mult)(inc, 2, 4).should.be.eql(15);
+      });
+    });
+    describe('(mult, inc)(2, 4)', () => {
+      it('== 6', () => {
+        Function.on(mult, inc)(2, 4).should.be.eql(15);
+      });
+    });
+    describe('(mult, inc, 2)(4)', () => {
+      it('== 6', () => {
+        Function.on(mult, inc, 2)(4).should.be.eql(15);
+      });
+    });
+    describe('(mult)(inc)(2, 4)', () => {
+      it('== 6', () => {
+        Function.on(mult)(inc)(2, 4).should.be.eql(15);
+      });
+    });
+    describe('(mult)(inc, 2)(4)', () => {
+      it('== 6', () => {
+        Function.on(mult)(inc, 2)(4).should.be.eql(15);
+      });
+    });
+    describe('(mult, inc)(2)(4)', () => {
+      it('== 6', () => {
+        Function.on(mult, inc)(2)(4).should.be.eql(15);
+      });
+    });
+    describe('(mult)(inc)(2)(4)', () => {
+      it('== 6', () => {
+        Function.on(mult)(inc)(2)(4).should.be.eql(15);
       });
     });
   });
