89d6d8aeb8596edd4d57dd7c0a3724ad075ee262
Erik Sutherland <erik.misu@gmail.com>
2017-03-29 22:05:03 -0700

    fix tests

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const Data = require('./Data');
-const System = require('./System');
-const Prelude = require('./Prelude');
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.System = exports.Prelude = exports.util = exports.Data = undefined;
 
-module.exports = {
-  Data,
-  System,
-  Prelude,
-}
+var _Data = require('./Data');
+
+var Data = _interopRequireWildcard(_Data);
+
+var _Prelude = require('./Prelude');
+
+var Prelude = _interopRequireWildcard(_Prelude);
+
+var _System = require('./System');
+
+var System = _interopRequireWildcard(_System);
+
+var _util = require('./util');
+
+var util = _interopRequireWildcard(_util);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+exports.Data = Data;
+exports.util = util;
+exports.Prelude = Prelude;
+exports.System = System;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as Data from './Data';
+import * as Prelude from './Prelude';
+import * as System from './System';
+import * as util from './util';
+
+export {
+  Data,
+  util,
+  Prelude,
+  System,
+};
