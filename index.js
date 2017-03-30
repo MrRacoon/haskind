db7647223e626f813a0cdd31b763cb8b68747b14
Erik Sutherland <erik.misu@gmail.com>
2017-03-29 21:48:35 -0700

    make requiring submodules cleaner

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./dist');
+'use strict';
+
+const Data = require('./Data');
+const System = require('./System');
+const Prelude = require('./Prelude');
+
+module.exports = {
+  Data,
+  System,
+  Prelude,
+}
